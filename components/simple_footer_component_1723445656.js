252ae9386cf2d918c5f0080011c58f86db36dccc
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-12 06:54:26 +0000

    Placeholder text replacement

--- components/simple_footer_component_1723445656.js
+++ components/simple_footer_component_1723445656.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1723445656", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            SlimLine
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Experience the transformative power of minimalism with SlimLine. Streamline your life, declutter your space, and embrace the essence of simplicity.
                     </div>
                 </div>
             </div>
